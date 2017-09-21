76696ac4fe7bb3b32c510c403d888857e2933159
David Williams <DavidW@mmsg.com.au>
2017-09-21 17:04:09 +1000

    Test check in.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedMeet: '',
+            selectedMeet: 'test',
             raceMeets: [],
             punters: [],
             tips: []
