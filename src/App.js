a208fba159463c3b679accab8169613dcb57a5e4
David Williams <david.williams@oakton.com.au>
2017-10-04 23:59:23 +1100

    Removing un-necessary import.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './components/Login';
 import Admin from './components/Admin';
 import Information from './components/Information';
